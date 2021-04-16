92e5a6ec20e4be45458239c008d7fe5e78c9c6a6
XanderCorversPXL <xander.corvers@student.pxl.be>
2021-04-16 15:37:32 +0200

    init

--- src/main/java/be/pxl/ja2/jdbc/InsertUpdateDelete.java
+++ src/main/java/be/pxl/ja2/jdbc/InsertUpdateDelete.java
@@ -7,34 +7,46 @@ import java.sql.*;
 
 public class InsertUpdateDelete {
 
-	private static final Logger LOGGER = LogManager.getLogger(InsertUpdateDelete.class);
+    private static final Logger LOGGER = LogManager.getLogger(InsertUpdateDelete.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb", "user", "password");
-		     Statement statement = conn.createStatement()) {
-			//conn.setAutoCommit(false);
-			statement.execute("INSERT INTO contacts (name, phone, email) " +
-					"VALUES('Joe', 45632, 'joe@anywhere.com')");
-			statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
-			statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb", "user", "password");
+             Statement statement = conn.createStatement()) {
+            //conn.setAutoCommit(false);
+            statement.execute("DELETE FROM contacts WHERE true");
+            statement.execute("INSERT INTO contacts (name, phone, email) " +
+                    "VALUES('Joe', 45632, 'joe@anywhere.com')");
+            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
+            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Fido', 9038, 'dog@email.com')");
 
-			statement.execute("SELECT * FROM contacts");
-			ResultSet results = statement.getResultSet();
-			int numberOfRows = 0;
-			while (results.next()) {
-				numberOfRows++;
-				System.out.println(results.getString("name") + " " +
-						results.getInt("phone") + " " +
-						results.getString("email"));
-			}
-			LOGGER.info("Number of rows in table: " + numberOfRows);
+            statement.execute("SELECT * FROM contacts");
+            //result of last select query on statement
+            ResultSet results = statement.getResultSet();
+            int numberOfRows = 0;
+            while (results.next()) {
+                numberOfRows++;
+                System.out.println(results.getString("name") + " " +
+                        results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
+            LOGGER.info("Number of rows in table: " + numberOfRows);
 
-			statement.executeUpdate("UPDATE contacts set phone='486666' WHERE name = 'Jane'");
+            int updatedRows = statement.executeUpdate("UPDATE contacts set phone = '486666' WHERE name = 'Jane'");
+            LOGGER.info("Rows updated: " + updatedRows);
 
-			statement.executeUpdate("DELETE FROM contacts WHERE email = 'dog@email.com'");
-		} catch (SQLException e) {
-			LOGGER.fatal("Something went wrong.", e);
-		}
-	}
+            statement.executeUpdate("DELETE FROM contacts WHERE email = 'dog@email.com'");
+
+            statement.execute("SELECT * FROM contacts");
+            results = statement.getResultSet();
+            while (results.next()) {
+                numberOfRows++;
+                System.out.println(results.getString("name") + " " +
+                        results.getInt("phone") + " " +
+                        results.getString("email"));
+            }
+        } catch (SQLException e) {
+            LOGGER.fatal("Something went wrong.", e);
+        }
+    }
 }
